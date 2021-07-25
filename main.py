9c1d58f1c7385eec14f6a2fe6775224b33ba3dec
Thisguy217 <87245214+Thisguy217@users.noreply.github.com>
2021-07-25 15:21:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,18 +7,93 @@ class Meme:
     def game():
         window=tk.Toplevel()
         window.title('What do you meme?')
-        window.geometry("1300x675")
+
+        def normal():
+            imgres=img.rotate(0)
+            width, height = imgres.size
+            if width>1012 or height>675:
+                imgsize=imgres.resize((1012,675))
+            elif width<216 or height<144:
+                imgres=img.resize((216,144))
+            else:
+                imgsize=imgres
+            imgfin=ImageTk.PhotoImage(imgsize)
+            canvas.configure(image=imgfin)
+            canvas.image=imgfin
+
+        def rotateright():
+            imgres=img.rotate(90)
+            width, height = imgres.size
+            if width>1012 or height>675:
+                imgsize=imgres.resize((1012,675))
+            elif width<216 or height<144:
+                imgres=img.resize((216,144))
+            else:
+                imgsize=imgres
+            imgfin=ImageTk.PhotoImage(imgsize)
+            canvas.configure(image=imgfin)
+            canvas.image=imgfin
+
+        def rotateleft():
+            imgres=img.rotate(270)
+            width, height = imgres.size
+            if width>1012 or height>675:
+                imgsize=imgres.resize((1012,675))
+            elif width<216 or height<144:
+                imgres=img.resize((216,144))
+            else:
+                imgsize=imgres
+            imgfin=ImageTk.PhotoImage(imgsize)
+            canvas.configure(image=imgfin)
+            canvas.image=imgfin
+
+        def flipup():
+            imgres=img.rotate(180)
+            width, height = imgres.size
+            if width>1012 or height>675:
+                imgsize=imgres.resize((1012,675))
+            elif width<216 or height<144:
+                imgres=img.resize((216,144))
+            else:
+                imgsize=imgres
+            imgfin=ImageTk.PhotoImage(imgsize)
+            canvas.configure(image=imgfin)
+            canvas.image=imgfin
+
+        button=tk.Button(window, text="Normal",command=lambda:normal())
+        button.grid(row=0,column=0,sticky="WENS")
+
+        button1=tk.Button(window,text="Rotate Left",command=lambda:rotateright())
+        button1.grid(row=0,column=1,sticky="WENS")
+
+        button2=tk.Button(window,text="Rotate Right", command=lambda:rotateleft())
+        button2.grid(row=0, column=2,sticky="WENS")
+
+        button3=tk.Button(window,text="Flip", command=lambda:flipup())
+        button3.grid(row=0, column=3,sticky="WENS")
+
+        canvas=tk.Label(window,text="")
+
+        #data pull
         pull=str(random.choice(os.listdir(Dir.get())))
         img=Image.open(Dir.get()+'/'+pull)
+        
+        #image size check
         width, height = img.size
-        if width>762 and height>700:
-            imgres=img.resize((762,700))
+        if width>1012 or height>675:
+            imgres=img.resize((1012,675))
+        elif width<216 or height<144:
+            imgres=img.resize((216,144))
         else:
             imgres=img
+
+        #image load
         imgfin=ImageTk.PhotoImage(imgres)
-        canvas=tk.Label(master=window,image=imgfin)
-        canvas.pack()
+        canvas.configure(image=imgfin)
+
+        canvas.grid(row=1,column=0,columnspan=4,sticky="WENS",padx=150)
         window.mainloop()
+
 
 root=tk.Tk()
 root.title('Picture Generator')
